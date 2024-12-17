735c9fec86d071e9a0b69e28040dfcc31f81744c
devtescode <teslimagboola09@gmail.com>
2024-12-17 18:10:49 +0100

    update register

--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import "./Signup.css"
 
 const Signup = () => {
+  const SignupBtn=()=>{
+    alert("Submit...")
+  }
   return (
-    <div>Signup</div>
+    <>
+    <div className='container_main'>
+      <div className='main'>
+        <input type="checkbox" id='chk' aria-hidden="true"/>
+        <div className="signup">
+          <form>
+            <label htmlFor="" for="chk" aria-hidden="true" className='labelstyle'>Sign Up</label>
+            <input className='input_text' type="text" placeholder='User name' required/>
+            <input className='input_text' type="text"  placeholder='Email' required/>
+            <input className='input_text' type="text"  placeholder='Phone' required/>
+            <input className='input_text' type="text" placeholder='Password' required/>
+            <button className='button_submit' onClick={SignupBtn}>Sign-Up</button>
+          </form>
+        </div>
+
+
+        <div className="login">
+          <form>
+            <label htmlFor="" for="chk" aria-hidden="true" className='labelstyle'>Login</label>
+            <input type="text" className='input_text'  placeholder='Email' required/>
+            <input type="text" className='input_text' placeholder='Password' required/>
+            <button className='button_submit'>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
