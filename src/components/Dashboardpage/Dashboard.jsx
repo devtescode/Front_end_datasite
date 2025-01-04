a8f27bd40489cfacdd7b91448d9708b271b08671
devtescode <teslimagboola09@gmail.com>
2025-01-04 09:52:41 +0100

    update dashboard page

--- src/components/Dashboardpage/Dashboard.jsx
+++ src/components/Dashboardpage/Dashboard.jsx
@@ -5,24 +5,66 @@ const Dashboard = () => {
     return (
         <>
             <Navbar />
-            <nav class="navbar navbar-expand-lg bg-body-tertiary  p-3">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary p-3">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">DATASUB</a>
+                    <a class="navbar-brand" style={{ cursor: "pointer" }}>DATASUB</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="collapse navbar-collapse justify-content-center gap-4" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Service
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
-                                    <li><a class="dropdown-item" href="#">Buy Data</a></li>
-                                    <li><a class="dropdown-item" href="#">Buy Airtime</a></li>
+                                    <li><a class="dropdown-item">Buy Data</a></li>
+                                    <li><a class="dropdown-item">Buy Airtime</a></li>
                                 </ul>
                             </li>
                         </ul>
+
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Pay Bills
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
+                                    <li><a class="dropdown-item">Cable Subscription</a></li>
+                                    <li><a class="dropdown-item">Electricity Payment</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    My Wallet
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
+                                    <li><a class="dropdown-item">Fund Wallet</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                        
+
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                   My Account
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
+                                    <li><a class="dropdown-item">My profile</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                        <a class="nav-link "  role="button" aria-expanded="false">
+                            LogOut
+                        </a>
+
+
                     </div>
                 </div>
             </nav>
