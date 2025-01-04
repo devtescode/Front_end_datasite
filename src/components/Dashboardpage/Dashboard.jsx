45281025248f72459994d64c003510c66ae80f97
devtescode <teslimagboola09@gmail.com>
2025-01-04 10:24:17 +0100

    add icon to dashboard

--- src/components/Dashboardpage/Dashboard.jsx
+++ src/components/Dashboardpage/Dashboard.jsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="ri-numbers-line" style={{color:"#00838d"}}></i> Service
+                                    <i class="ri-numbers-line" style={{ color: "#00838d" }}></i> Service
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
                                     <li><a class="dropdown-item">Buy Data</a></li>
@@ -27,7 +27,7 @@ const Dashboard = () => {
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="ri-flashlight-fill" style={{color:"#00838d"}}></i> Pay Bills
+                                    <i class="ri-flashlight-fill" style={{ color: "#00838d" }}></i> Pay Bills
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
                                     <li><a class="dropdown-item">Cable Subscription</a></li>
@@ -39,7 +39,7 @@ const Dashboard = () => {
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="ri-wallet-line" style={{color:"#00838d"}}></i> My Wallet
+                                    <i class="ri-wallet-line" style={{ color: "#00838d" }}></i> My Wallet
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
                                     <li><a class="dropdown-item"> Fund Wallet</a></li>
@@ -47,12 +47,12 @@ const Dashboard = () => {
                             </li>
                         </ul>
 
-                        
+
 
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="ri-user-fill" style={{color:"#00838d"}}></i>  My Account
+                                    <i class="ri-user-fill" style={{ color: "#00838d" }}></i>  My Account
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
                                     <li><a class="dropdown-item">My profile</a></li>
@@ -60,10 +60,14 @@ const Dashboard = () => {
                             </li>
                         </ul>
 
-                        <a class="nav-link "  role="button" aria-expanded="false">
-                        <i class="ri-logout-box-r-line" style={{color:"#00838d"}}></i> LogOut
+                        <a class="nav-link " role="button" aria-expanded="false">
+                            <i class="ri-history-line" style={{ color: "#00838d" }}></i> Transactions History
                         </a>
 
+
+                        <a class="nav-link " role="button" aria-expanded="false">
+                            <i class="ri-logout-box-r-line" style={{ color: "#00838d" }}></i> LogOut
+                        </a>
 
                     </div>
                 </div>
