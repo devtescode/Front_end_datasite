528ba8ea2b805a21545b1c2436a8f4534bb39d64
devtescode <teslimagboola09@gmail.com>
2025-01-06 12:47:28 +0100

    update dashboard page

--- src/components/Dashboardpage/Dashboard.jsx
+++ src/components/Dashboardpage/Dashboard.jsx
@@ -8,74 +8,122 @@ const Dashboard = () => {
             <nav class="navbar navbar-expand-lg bg-body-tertiary p-3">
                 <div class="container-fluid">
                     <a class="navbar-brand" style={{ cursor: "pointer" }}>DATASUB</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasNavbar"
+                        aria-controls="offcanvasNavbar"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse justify-content-center gap-4" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="ri-numbers-line" style={{ color: "#00838d" }}></i> Service
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
-                                    <li><a class="dropdown-item">Buy Data</a></li>
-                                    <li><a class="dropdown-item">Buy Airtime</a></li>
-                                </ul>
-                            </li>
-                        </ul>
 
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="ri-flashlight-fill" style={{ color: "#00838d" }}></i> Pay Bills
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
-                                    <li><a class="dropdown-item">Cable Subscription</a></li>
-                                    <li><a class="dropdown-item">Electricity Payment</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="ri-wallet-line" style={{ color: "#00838d" }}></i> My Wallet
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
-                                    <li><a class="dropdown-item"> Fund Wallet</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-
-
-
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="serviceDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="ri-user-fill" style={{ color: "#00838d" }}></i>  My Account
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
-                                    <li><a class="dropdown-item">My profile</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-
-                        <div>
-                            <a class="nav-link " role="button" aria-expanded="false">
-                                <i class="ri-history-line" style={{ color: "#00838d" }}></i> History
-                            </a>
+                    {/* Offcanvas */}
+                    <div
+                        class="offcanvas offcanvas-start w-75"
+                        tabindex="-1"
+                        id="offcanvasNavbar"
+                        aria-labelledby="offcanvasNavbarLabel"
+                    >
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">DATASUB</h5>
+                            <button
+                                type="button"
+                                class="btn-close text-reset"
+                                data-bs-dismiss="offcanvas"
+                                aria-label="Close"
+                            ></button>
                         </div>
+                        <div class="offcanvas-body justify-content-center">
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                    <a
+                                        class="nav-link dropdown-toggle"
+                                        id="serviceDropdown"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        <i class="ri-numbers-line" style={{ color: "#00838d" }}></i> Service
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
+                                        <li><a class="dropdown-item">Buy Data</a></li>
+                                        <li><a class="dropdown-item">Buy Airtime</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
 
-                        <div>
-                            <a class="nav-link " role="button" aria-expanded="false">
-                                <i class="ri-notification-line" style={{ color: "#00838d" }}></i> Notification
-                            </a>
-                        </div>
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                    <a
+                                        class="nav-link dropdown-toggle"
+                                        id="serviceDropdown"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        <i class="ri-flashlight-fill" style={{ color: "#00838d" }}></i> Pay Bills
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
+                                        <li><a class="dropdown-item">Cable Subscription</a></li>
+                                        <li><a class="dropdown-item">Electricity Payment</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
 
-                        <div>
-                            <a class="nav-link " role="button" aria-expanded="false">
-                                <i class="ri-logout-box-r-line" style={{ color: "#00838d" }}></i> LogOut
-                            </a>
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                    <a
+                                        class="nav-link dropdown-toggle"
+                                        id="serviceDropdown"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        <i class="ri-wallet-line" style={{ color: "#00838d" }}></i> My Wallet
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
+                                        <li><a class="dropdown-item">Fund Wallet</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                    <a
+                                        class="nav-link dropdown-toggle"
+                                        id="serviceDropdown"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        <i class="ri-user-fill" style={{ color: "#00838d" }}></i> My Account
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="serviceDropdown">
+                                        <li><a class="dropdown-item">My profile</a></li>
+                                    </ul>
+                                </li>
+                                <div>
+                                    <a class="nav-link" role="button" aria-expanded="false">
+                                        <i class="ri-history-line" style={{ color: "#00838d" }}></i> History
+                                    </a>
+                                </div>
+
+                                <div>
+                                    <a class="nav-link" role="button" aria-expanded="false">
+                                        <i class="ri-notification-line" style={{ color: "#00838d" }}></i> Notification
+                                    </a>
+                                </div>
+
+                                <div>
+                                    <a class="nav-link" role="button" aria-expanded="false">
+                                        <i class="ri-logout-box-r-line" style={{ color: "#00838d" }}></i> LogOut
+                                    </a>
+                                </div>
+                            </ul>
+
                         </div>
                     </div>
                 </div>
@@ -126,7 +174,7 @@ const Dashboard = () => {
                             Reseller
                         </div>
                         <div className='text-start hover-underline' style={{ cursor: "pointer" }}>
-                            <span style={{color:"#00838d"}}>Referral link</span><span className='mx-1'>Click here</span>
+                            <span style={{ color: "#00838d" }}>Referral link</span><span className='mx-1'>Click here</span>
                         </div>
                     </div>
 
